507750757a58e89d3367208c88f1f9303085ea31
ndy2 <emrdbs12@gmail.com>
2022-02-18 16:08:46 +0900

    5주차 study - 접근지정자

---
+++ live-study/src/test/java/week5/study/UsersTest.java
@@ -0,0 +1,20 @@
+package week5.study;
+
+import org.junit.jupiter.api.Test;
+
+class UsersTest {
+
+    @Test
+    void accessSpecifierTest(){
+        Users users = new Users();
+//        users.username = "홍길동";
+//        String username = users.username;
+    }
+
+    @Test
+    void getterSetterTest(){
+        Users users = new Users();
+        users.setUsername("홍길동");
+        String username = users.getUsername();
+    }
+}
